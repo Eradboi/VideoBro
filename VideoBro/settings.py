7c226530cac078687f36e5eb594230d6d8c7efc6
Erad Creates <120324954+Eradboi@users.noreply.github.com>
2024-11-18 17:53:53 +0100

    Update settings.py

--- VideoBro/settings.py
+++ VideoBro/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = "postgresql://postgres:FF6hFYdOfbrLyZD3ULOA@containers-us-west-115.railway.app:6589/railway"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+_g+3nv0jg+nj7=@ruq$qz5%eu(f5o(c#+-h3@r&h56f*g*l$a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['videobro-production.up.railway.app']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
